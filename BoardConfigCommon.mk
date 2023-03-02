98b9420a38c98c3f0a1b82ad8dbfe51b2b63778a
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:29:12 +0200

    sm8150-common: BoardConfigCommon: Conditionally enable AVB
    Co-authored-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -177,8 +177,14 @@ include $(COMMON_PATH)/sepolicy/SEPolicy.mk
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_verification_disabled_flag
+ifneq (user,$(TARGET_BUILD_VARIANT))
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+else ifneq (,$(wildcard .android-certs/releasekey.key))
+BOARD_AVB_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_KEY_PATH := .android-certs/releasekey.key
+else
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+endif
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
