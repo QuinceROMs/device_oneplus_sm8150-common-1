9dc356fa4266f19adcc24c9074a41f7ea30b4732
Codecity001 <sanganiharish263@gmail.com>
2024-06-24 12:15:49 -0500

    BoardConfigCommon: Adapt conditional AVB keys to the new path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,9 +167,9 @@ include $(COMMON_PATH)/sepolicy/SEPolicy.mk
 BOARD_AVB_ENABLE := true
 ifneq (user,$(TARGET_BUILD_VARIANT))
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-else ifneq (,$(wildcard .android-certs/releasekey.key))
+else ifneq (,$(wildcard vendor/derp/signing/keys/releasekey.key))
 BOARD_AVB_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_KEY_PATH := .android-certs/releasekey.key
+BOARD_AVB_KEY_PATH := vendor/derp/signing/keys/releasekey.key
 else
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 endif
