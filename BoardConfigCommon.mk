e0a6ea45265bb9465449d34d9aede02f0cf7e5ab
Codecity001 <sanganiharish263@gmail.com>
2024-06-24 12:15:49 -0500

    BoardConfigCommon: Adapt conditional AVB keys to the new path
    Co-authored: QuinceROMs <quinceroms@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,9 +182,9 @@ include $(COMMON_PATH)/sepolicy/SEPolicy.mk
 BOARD_AVB_ENABLE := true
 ifneq (user,$(TARGET_BUILD_VARIANT))
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-else ifneq (,$(wildcard .android-certs/releasekey.key))
+else ifneq (,$(wildcard vendor/pixelage-priv/config/releasekey.key))
 BOARD_AVB_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_KEY_PATH := .android-certs/releasekey.key
+BOARD_AVB_KEY_PATH := vendor/pixelage-priv/config/releasekey.key
 else
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 endif
