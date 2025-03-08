59392da45a7e18a014a0f4e08fbcf8ee498fb075
Bruno Martins <bgcngm@gmail.com>
2025-03-08 11:30:58 +0000

    sm8150-common: Move to reference fastboot aidl service
    Oneplus is also using it since sm8650.
    
    Change-Id: I206d23420952244c40cc73d384c2e8089254f4ae

--- common.mk
+++ common.mk
@@ -145,7 +145,7 @@ PRODUCT_PACKAGES += \
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl.custom \
+    android.hardware.fastboot-service.example_recovery \
     fastbootd
 
 # Fingerprint
