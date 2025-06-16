f72378b89338cc6be600b4aab769305518b93816
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    sm8150-common: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567

--- common.mk
+++ common.mk
@@ -20,10 +20,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     otapreopt_script
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong
-
 # Alert slider
 PRODUCT_PACKAGES += \
     KeyHandler \
