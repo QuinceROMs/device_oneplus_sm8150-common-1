3d1b8a1230c708cd59145c6b42d92fadc4f8b774
Quince <quinceroms@gmail.com>
2026-02-21 23:43:32 +0100

    sm8150-common: Remove libcamera_metadata_shim fixup for chi.override
    The OOS 12.1 H.41 version of com.qti.chi.override.so no longer links
    against libcamera_metadata_shim.so, so the blob_fixup add_needed entry
    is no longer required.
    
    Signed-off-by: Quince <quinceroms@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -79,8 +79,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('<HID>0</HID>', '<HID>1</HID>'),
     'vendor/lib/hw/sound_trigger.primary.msmnile.so': blob_fixup()
         .binary_regex_replace(b'/vendor/lib/hw\x00', b'/odm/lib/hw\x00\x00\x00\x00'),
-    'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
-        .add_needed('libcamera_metadata_shim.so'),
     ('vendor/lib64/libdlbdsservice.so', 'vendor/lib/libstagefright_soft_ac4dec.so', 'vendor/lib/libstagefright_soft_ddpdec.so'): blob_fixup()
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     'vendor/lib64/libdpps.so': blob_fixup()
