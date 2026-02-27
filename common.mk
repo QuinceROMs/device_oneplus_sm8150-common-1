3fc3b4bb2fe97d362aa3a9ff2fd7ffc35f6bd958
Quince <quinceroms@gmail.com>
2026-02-27 17:51:45 +0100

    Reapply "sm8150-common: Switch to NCM USB tethering"
    This reverts commit 81820f257fc283016a9ba89d6963af49c865ae88.
    
    Requires the following kernel backports for stable NCM operation:
    - 2899af532c3d ("BACKPORT: usb: gadget: f_ncm: remove timer_force_tx field")
    - 88fc0f48fa3f ("BACKPORT: usb: gadget: f_ncm: remove spurious boolean timer_stopping")
    - 5e9f6c655d7a ("BACKPORT: usb: gadget: f_ncm: Fix UAF ncm object at re-bind after usb ep transport error")
    - e453343bfb2a ("BACKPORT: usb: gadget: f_ncm: set SuperSpeed bulk descriptor bMaxBurst to 15")
    - 9452fcb39e10 ("usb: gadget: f_ncm: guard ncm_wrap_ntb error path against NULL netdev")
    - 89a5d5410855 ("BACKPORT: usb: gadget: f_ncm: only first packet of aggregate needs to start timer")
    - 409ad4a6830d ("BACKPORT: usb: gadget: f_ncm: remove check for NULL skb_tx_data in timer function")
    - ae7f030cf02d ("BACKPORT: usb: gadget: f_ncm: remove spurious if statement")
    - f8acd7b254eb ("BACKPORT: usb: gadget: f_ncm: ncm_wrap_ntb - move var definitions into if statement")
    - e8269906d4cd ("BACKPORT: usb: gadget: f_ncm: Fix MAC assignment NCM ethernet")
    - 682e6d34df1b ("BACKPORT: usb: gadget: f_ncm: align net_device lifecycle with bind/unbind")
    - ebf8e087f98b ("BACKPORT: usb: gadget: u_ether: don't starve tx request queue")
    - 3aa995f1aa57 ("usb: gadget: f_ncm: fix use-after-free in ncm_wrap_ntb error path")
    
    Signed-off-by: Quince <quinceroms@gmail.com>

--- common.mk
+++ common.mk
@@ -304,6 +304,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
+    NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
     OPlusSystemUIResCommon \
