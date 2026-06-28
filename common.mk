3d2efaa81a4bbd9a88ff97116d103e3c141bdc95
Quince <quinceroms@gmail.com>
2026-06-28 21:15:20 +0200

    sm8150-common: Wire libwfd_shim and libaudiobase into libwfdmmsrc_system
    Add the createVirtualDisplay shim and libaudiobase to the relinked blob
    and install the shim.
    
    Signed-off-by: Quince <quinceroms@gmail.com>

--- common.mk
+++ common.mk
@@ -460,6 +460,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_ODM)/vendor/etc/wifi/WCNSS_qcom_cfg.ini
 
+# WiFi display
+PRODUCT_PACKAGES += \
+    libwfd_shim
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
