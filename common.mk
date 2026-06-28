677707abda25a592c990e10a39d5ac2470d7dc4e
Quince <quinceroms@gmail.com>
2026-06-28 14:42:10 +0200

    sm8150-common: Drop android.frameworks.displayservice@1.0
    The HIDL displayservice interface was removed in Android 17, leaving
    PRODUCT_PACKAGES referencing a non-existent module.
    
    Signed-off-by: Quince <quinceroms@gmail.com>

--- common.mk
+++ common.mk
@@ -99,7 +99,6 @@ $(call soong_config_set_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     OnePlusCameraService \
