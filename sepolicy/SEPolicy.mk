116743f5050eb3c2b00321f032ec2240b523261c
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-02 19:06:32 +0200

    sm8150-common: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -6,3 +6,5 @@
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
+
+include device/lineage/sepolicy/libion/sepolicy.mk
