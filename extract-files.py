c5747065452d68e359683b66b0e4964c10daad3c
LuK1337 <priv.luk@gmail.com>
2026-07-06 21:12:19 +0200

    sm8150-common: Bump SNS registry version
    Apparently some users have invalid sensors in their /mnt/vendor/persist,
    bump version to trigger database rebuild on boot.
    
    Change-Id: I2ef2a8ee00d2c2874665d9d5a4850a401c4771ee

--- extract-files.py
+++ extract-files.py
@@ -82,6 +82,8 @@ blob_fixups: blob_fixups_user_type = {
         .patch_file('blob-patches/vendor.sensors.sscrpcd.rc.patch'),
     'vendor/etc/init/vppservice.rc': blob_fixup()
         .patch_file('blob-patches/vppservice.rc.patch'),
+    'vendor/etc/sensors/sns_reg_config': blob_fixup()
+        .regex_replace('version=8', 'version=9'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=0x01', 'NFC_DEBUG_ENABLED=0x00'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
